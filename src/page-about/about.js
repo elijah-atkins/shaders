62abc4146fff7a69efd0b0c74aa63a8a1f746b6a
Elijah Atkins <59714012+elijah-atkins@users.noreply.github.com>
2022-04-24 20:40:52 -0600

    Initial commit

---
+++ src/page-about/about.js
@@ -0,0 +1,4 @@
+import '../css/main.css'
+import './about.css'
+
+console.log('this is about page');
---
+++ src/page-index/index.js
@@ -0,0 +1,9 @@
+import {Bar} from '../js/bar'
+import '../css/main.css'
+import './index.css'
+
+console.log('this is index/home page');
+
+window.Bar = Bar()
+
+console.log(window.Bar)
